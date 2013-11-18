d2cdc8f1e5127717a6ad819622bec7f295f02c02
Stephan Maximilian Huber <hello@stephanmaximilianhuber.com>
2013-11-18 11:37:52 +0100

    started with wrapper-app

---
+++ Present3D/Present3D/P3DAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  P3DAppDelegate.h
+//  Present3D
+//
+//  Created by Stephan Huber on 18.11.13.
+//  Copyright (c) 2013 OpenSceneGraph. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface P3DAppDelegate : NSObject <NSApplicationDelegate>
+
+-(IBAction) openFile: (id)sender;
+-(void) startAppWithFile: (NSURL*) file_name;
+
+@property (assign) IBOutlet NSWindow *window;
+
+
+@end
---
+++ Present3D/Present3D/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Present3D
+//
+//  Created by Stephan Huber on 18.11.13.
+//  Copyright (c) 2013 OpenSceneGraph. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
