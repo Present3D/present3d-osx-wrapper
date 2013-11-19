86391bb2d164e48859a2a85eb332451c0e4ef0ad
Stephan Maximilian Huber <hello@stephanmaximilianhuber.com>
2013-11-19 11:16:30 +0100

    added build-script for dcmtk

---
+++ vendor/build-dcmtk.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+ROOT=${PWD}
+
+# create cmake-configuration for dcmtk
+mkdir -p "${ROOT}/dcmtk-build/install"
+cd ${ROOT}/dcmtk-build
+/usr/bin/cmake -G Xcode \
+-D CMAKE_INSTALL_PREFIX:PATH=${ROOT}/dcmtk-build/install \
+-D DCMTK_WITH_TIFF:BOOL=OFF \
+-D CMAKE_OSX_SYSROOT:STRING=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk ../dcmtk-source
+
+#build dcmtk
+/usr/bin/xcodebuild -configuration "Release" -target "ALL_BUILD"
+/usr/bin/xcodebuild -configuration "Release" -target "install"
