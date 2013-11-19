c4a4e523cc28fb8655bc3bad463723eddb299cf9
Stephan Maximilian Huber <hello@stephanmaximilianhuber.com>
2013-11-19 12:56:04 +0100

    fixed dcmtk-build

--- vendor/build-dcmtk.sh
+++ vendor/build-dcmtk.sh
@@ -8,6 +8,8 @@ cd ${ROOT}/dcmtk-build
 /usr/bin/cmake -G Xcode \
 -D CMAKE_INSTALL_PREFIX:PATH=${ROOT}/dcmtk-build/install \
 -D DCMTK_WITH_TIFF:BOOL=OFF \
+-D DCMTK_WITH_ICONV:BOOL=OFF \
+-D DCMTK_WITH_OPENSSL:BOOL=OFF \
 -D CMAKE_OSX_SYSROOT:STRING=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk ../dcmtk-source
 
 #build dcmtk
