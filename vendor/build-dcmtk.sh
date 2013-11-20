d845a5542a36ef1d606807fd4bccd6a9f227890a
Stephan Maximilian Huber <hello@stephanmaximilianhuber.com>
2013-11-20 18:44:01 +0100

    added tiff-build

--- vendor/build-dcmtk.sh
+++ vendor/build-dcmtk.sh
@@ -2,14 +2,18 @@
 
 ROOT=${PWD}
 
+TIFF_ROOT=${ROOT}/tiff-build/install
+
 # create cmake-configuration for dcmtk
 mkdir -p "${ROOT}/dcmtk-build/install"
 cd ${ROOT}/dcmtk-build
 /usr/bin/cmake -G Xcode \
 -D CMAKE_INSTALL_PREFIX:PATH=${ROOT}/dcmtk-build/install \
--D DCMTK_WITH_TIFF:BOOL=OFF \
+-D DCMTK_WITH_TIFF:BOOL=ON \
 -D DCMTK_WITH_ICONV:BOOL=OFF \
 -D DCMTK_WITH_OPENSSL:BOOL=OFF \
+-D TIFF_INCLUDE_DIR:PATH=${TIFF_ROOT}/include \
+-D TIFF_LIBRARY:FILEPATH=${TIFF_ROOT}/lib/libtiff.a \
 -D CMAKE_OSX_SYSROOT:STRING=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk ../dcmtk-source
 
 #build dcmtk
---
+++ vendor/build-tiff.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+ROOT=${PWD}
+
+# create cmake-configuration for dcmtk
+mkdir -p "${ROOT}/tiff-build/install"
+rm -rf "${ROOT}/tiff-source"
+mkdir "${ROOT}/tiff-source"
+cd "${ROOT}/tiff-source"
+wget http://download.osgeo.org/libtiff/tiff-4.0.3.tar.gz
+/usr/bin/tar xf tiff-4.0.3.tar.gz
+cd tiff-4.0.3
+./configure --disable-lzma --prefix=${ROOT}/tiff-build/install
+make
+make install
