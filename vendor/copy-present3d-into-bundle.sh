6f75f5e6029633b834e8b976c25d50c28a7e82b9
Stephan Maximilian Huber <hello@stephanmaximilianhuber.com>
2013-11-22 18:38:18 +0100

    adjust dependencies

--- vendor/copy-present3d-into-bundle.sh
+++ vendor/copy-present3d-into-bundle.sh
@@ -1,18 +1,29 @@
 #!/bin/sh
 
-ROOT="${PWD}/../vendor/osg-build"
-echo ${ROOT}
-cd ${ROOT}/lib
+OSG_VERSION="3.3.1"
+
+
+ROOT="${PWD}/../vendor/"
+
+cd ${ROOT}/osg-build/lib
 
 #copying osg-plugins
 mkdir -p ${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.app/Contents/PlugIns
-cp -a osgPlugins* ${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.app/Contents/PlugIns/
+cp -a osgPlugins-${OSG_VERSION} ${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.app/Contents/PlugIns/
 
 #copying frameworks
 mkdir -p ${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.app/Contents/Frameworks
 cp -a *.framework ${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.app/Contents/Frameworks/
 
 # copying present3d-app
-cd ${ROOT}/bin
+cd ${ROOT}/osg-build/bin
 cp present3d ${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.app/Contents/PlugIns/
 
+# copy 3rdparty lubs
+
+cd ${ROOT}/libvnc-build/install/lib
+cp libvncclient.dylib ${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.app/Contents/Frameworks/
+
+
+#fix dependencies
+install_name_tool -change libvncclient.dylib @executable_path/../Frameworks/libvncclient.dylib  ${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.app/Contents/PlugIns/osgPlugins-${OSG_VERSION}/osgdb_vnc.so
