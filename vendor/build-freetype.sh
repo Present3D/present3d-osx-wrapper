e7ef2aafac8b63aa73b486754018319081ecd0b0
Stephan Maximilian Huber <hello@stephanmaximilianhuber.com>
2014-01-07 10:24:54 +0100

    removed unnecessary install subfolder

--- vendor/build-freetype.sh
+++ vendor/build-freetype.sh
@@ -3,13 +3,13 @@
 ROOT=${PWD}
 
 # create cmake-configuration for dcmtk
-mkdir -p "${ROOT}/freetype-build/install"
+mkdir -p "${ROOT}/freetype-build"
 rm -rf "${ROOT}/freetype-source"
 mkdir "${ROOT}/freetype-source"
 cd "${ROOT}/freetype-source"
 curl --location --output freetype.tar.gz http://download.savannah.gnu.org/releases/freetype/freetype-2.5.2.tar.gz
 /usr/bin/tar xf freetype.tar.gz
 cd freetype-2.5.2
-./configure --prefix=${ROOT}/freetype-build/install --without-png
+./configure --prefix=${ROOT}/freetype-build --without-png
 make
 make install
